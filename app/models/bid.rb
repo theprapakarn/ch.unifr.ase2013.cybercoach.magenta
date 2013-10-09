4a2de1dce235238bb341e3787617be84abc6d4c6
theprapakarn <theprapakarn@gmail.com>
2013-10-09 20:08:24 +0200

    Version 0.0.1

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -6,6 +6,7 @@ class Bid < ActiveRecord::Base
   validates_numericality_of :price,
                             only_decimal: true,
                             allow_blank: false,
+                            greater_than: 0,
                             greater_than: ->(bid) { bid.car.price },
                             greater_than: ->(bid) {
                               if !bid.car.bids.last.nil?
--- app/models/car.rb
+++ app/models/car.rb
@@ -3,12 +3,14 @@ class Car < ActiveRecord::Base
   has_many :bids, dependent: :destroy
   validates :user_id, :brand, :model, presence: true
   default_scope -> { order('created_at DESC') }
-  validates_numericality_of :price, only_decimal: true, allow_blank: false
+  validates_numericality_of :price, only_decimal: true,
+                            allow_blank: false,
+                            greater_than: 0
 
   def self.search(search)
     if search
-      where('LOWER(model) LIKE ? OR LOWER(brand) LIKE ?', "%#{search.downcase}%", "%#{search.downcase}%" ).order("created_at DESC")
-     else
+      where('LOWER(model) LIKE ? OR LOWER(brand) LIKE ?', "%#{search.downcase}%", "%#{search.downcase}%").order("created_at DESC")
+    else
       find(:all, :order => 'created_at  desc')
     end
   end

4ba3b2df76a7788c881e561a15f097741d8065f0
PATT-XUBUNTU-DEVELOPMENT <theprapakarn@gmail.com>
2013-12-13 10:02:25 +0100

    Fix KendoUi and Bootstrapon Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ CarTrading::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   Rails.logger = Logger.new(STDOUT)
 
   config.log_level = :warn # In any environment initializer, or
